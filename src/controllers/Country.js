293b35466585eae95d61b0d22f97a85f0228297c
darshan-printf <darshan.talaviya@egrambook.com>
2025-02-22 11:30:23 +0530

    country detele api add

--- src/controllers/Country.js
+++ src/controllers/Country.js
@@ -1,5 +1,6 @@
 import Country from "../models/Country.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import State from "../models/States.js";
 
 // Create a new country
 export const createCountry = asyncHandler(async (req, res) => {
@@ -37,4 +38,30 @@ export const updateCountry = asyncHandler(async (req, res) => {
     }
     res.json(country);
 });
+
+
+
+// Delete a country by ID
+export const deleteCountry = asyncHandler(async (req, res) => {
+    const country = await Country.findById(req.params.id);
+    
+    if (!country) {
+        return res.status(404).json({ message: "Country not found" });
+    }
+
+    // Check if any states reference this country
+    const relatedStates = await State.find({ country: req.params.id });
+
+
+    if (relatedStates.length > 0) {
+        return res.status(400).json({ message: "Cannot delete country with related states. Delete related states first." });
+    }
+
+    // Delete the country
+    await Country.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "Country deleted successfully" });
+});
+
+
 
--- src/routes/CountryRoutes.js
+++ src/routes/CountryRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCountry, getAllCountries, getCountryById, updateCountry } from "../controllers/Country.js";
+import { createCountry, deleteCountry, getAllCountries, getCountryById, updateCountry } from "../controllers/Country.js";
 import protect from "../middleware/authMiddleware.js";
 
 const countryRoute = Router();
@@ -9,5 +9,6 @@ countryRoute.post("/add",protect,createCountry);
 countryRoute.get("/get",protect,getAllCountries);
 countryRoute.get('/get/:id',protect,getCountryById);
 countryRoute.put('/update',protect,updateCountry);
+countryRoute.delete('/delete/:id',protect,deleteCountry);
 
 export default countryRoute;
