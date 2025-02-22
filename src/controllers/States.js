41c65eb49c17b91163ec6d0d2ff8641df7341b4a
darshan-printf <darshan.talaviya@egrambook.com>
2025-02-22 11:37:29 +0530

    Add states crud api

--- src/controllers/States.js
+++ src/controllers/States.js
@@ -1,9 +1,45 @@
 import State from "../models/States.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-
+// carte  new state 
 export const createState = asyncHandler(async (req, res) => {
     const { name, code, countryId } = req.body;
     const state = await State.create({ name, code, country:countryId});
     res.status(201).json(state);
+});
+
+// get all states
+export const getAllStates = asyncHandler(async (req, res) => {
+    const states = await State.find({});
+    res.json(states);
+});
+
+// get state by id
+export const getStateById = asyncHandler(async (req, res) => {
+    const state = await State.findById(req.params.id);
+
+    if (!state) {
+        return res.status(404).json({ message: "State not found" });
+    }
+
+    res.json(state);
+});
+
+// update state by id
+export const updateState = asyncHandler(async (req, res) => {   
+    const state = await State.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!state) {
+        return res.status(404).json({ message: "State not found" });
+    }
+    res.json(state);
+});
+
+// delete state by id
+export const deleteState = asyncHandler(async (req, res) => {
+    const state = await State.findById(req.params.id);
+    if (!state) {
+        return res.status(404).json({ message: "State not found" });
+    }
+    await state.remove();
+    res.json({ message: "State deleted successfully" });
 });
--- src/routes/StatesRoutes.js
+++ src/routes/StatesRoutes.js
@@ -1,11 +1,12 @@
-import { createState } from "../controllers/States.js";
+import { createState, deleteState, getAllStates, getStateById, updateState } from "../controllers/States.js";
 import protect from "../middleware/authMiddleware.js";
 import { Router } from "express";
-
-
 const stateRoute = Router();
 
-
 stateRoute.post("/add",protect,createState);
+stateRoute.get("/get",protect,getAllStates);  
+stateRoute.get('/get/:id',protect,getStateById);  
+stateRoute.put('/update',protect,updateState);  
+stateRoute.delete('/delete/:id',protect,deleteState);
 
 export default stateRoute;
