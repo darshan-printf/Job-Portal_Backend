ef93051a632309ebc3240933cf1c0b78344aa182
darshan-printf <darshan.talaviya@egrambook.com>
2025-06-25 13:19:03 +0530

    feat: enhance user update route to support file uploads for profile image and logo

--- src/routes/User.js
+++ src/routes/User.js
@@ -2,13 +2,22 @@ import protect from "../middleware/authMiddleware.js";
 import { Router } from "express";
 import { authorize } from "../middleware/authMiddleware.js";
 import { activeStatus, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/User.js";
+import upload from "../middleware/uploadMiddleware.js";
 
 const userRoute = Router();
 
 userRoute.get("/get",protect,authorize("admin"),getAllUsers);
 userRoute.get('/get/:id',protect,authorize("admin"),getUserById);
-userRoute.put('/update',protect,authorize("admin"),updateUser);
-userRoute.delete('/delete/:id',protect,authorize("admin"),deleteUser);
+userRoute.put(
+  '/update',
+  protect,
+  authorize("admin"),
+  upload.fields([
+    { name: 'profileImage', maxCount: 1 },
+    { name: 'logo', maxCount: 1 }
+  ]),
+  updateUser
+);userRoute.delete('/delete/:id',protect,authorize("admin"),deleteUser);
 userRoute.put('/active/:id',protect,authorize("admin"),activeStatus);
 
 
