9858c9dac5c232a62e274ba780d6d01deff49f29
darshan-printf <darshan.talaviya@egrambook.com>
2025-09-19 18:01:37 +0530

    feat(admin): add password change functionality for admin users

--- src/controllers/Admin.js
+++ src/controllers/Admin.js
@@ -2,6 +2,7 @@ import Admin from "../models/Admin.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import mongoose from "mongoose";
 import imageToBase64 from "../utils/imageToBase64.js";
+import bcrypt from "bcryptjs";
 
 // admin update profile api
 export const updateProfile = asyncHandler(async (req, res) => {
@@ -65,4 +66,33 @@ export const getAdminById = asyncHandler(async (req, res) => {
   res.json(adminObj);
 });
 
+// 
+export const changePassword = asyncHandler(async (req, res) => {
+  const { id, oldPassword, newPassword } = req.body;
 
+  if (!id || !oldPassword || !newPassword) {
+    return res.status(400).json({ message: "Admin ID, old password and new password are required" });
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid Admin ID format" });
+  }
+
+  const admin = await Admin.findById(id).select("+password"); // ensure password field is included
+  if (!admin) {
+    return res.status(404).json({ message: "Admin not found" });
+  }
+
+  // check old password
+  const isMatch = await bcrypt.compare(oldPassword, admin.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Old password is incorrect" });
+  }
+
+  // hash new password
+  const salt = await bcrypt.genSalt(10);
+  admin.password = await bcrypt.hash(newPassword, salt);
+  await admin.save();
+
+  res.json({ message: "Password changed successfully" });
+});
--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import protect from "../middleware/authMiddleware.js";
 import { authorize } from "../middleware/authMiddleware.js";
-import { getAdminById, updateProfile } from "../controllers/Admin.js";
+import { changePassword, getAdminById, updateProfile } from "../controllers/Admin.js";
 import upload from "../middleware/uploadMiddleware.js";
 
 const adminRouter = Router();
 
 adminRouter.post("/updateProfile",protect,authorize("admin"), upload.fields([  { name: 'profileImage', maxCount: 1 }]), updateProfile);
 adminRouter.get("/get/:id",protect,authorize("admin"),getAdminById);
+adminRouter.put("/changepassword" ,protect ,authorize("admin"),changePassword)
 
 
 export default adminRouter;
