b2f6b6539778f6f049f7b3ac5a03d30f13b72a58
darshan-printf <darshan.talaviya@egrambook.com>
2025-09-26 11:41:03 +0530

    add job apis for user

--- src/routes/JobRoutes.js
+++ src/routes/JobRoutes.js
@@ -8,9 +8,9 @@ const jobRoute = Router();
 
 jobRoute.post("/addAdmin",protect, authorize("admin"), addJobAdmin);
 jobRoute.get("/get",protect, authorize("admin"), getAllJobs);
-jobRoute.get("/get/:id",protect, authorize("admin"), getJobById);
-jobRoute.delete("/delete/:id",protect, authorize("admin"), deleteJob);
-jobRoute.put("/update",protect, authorize("admin"), updateJob);
+jobRoute.get("/get/:id",protect, authorize("admin", "user"), getJobById);
+jobRoute.delete("/delete/:id",protect, authorize("admin", "user"), deleteJob);
+jobRoute.put("/update",protect, authorize("admin","user"), updateJob);
 
 // add job user
 jobRoute.post("/addPost",protect, authorize("user"), addJobUser);
