2699ead62fc748af5aca029dcb59f51eb30180d5
darshan-printf <darshan.talaviya@egrambook.com>
2025-10-07 12:27:41 +0530

    refactor: remove education-related files and routes
    The education feature was removed as it's no longer needed in the application. This includes deleting the model, controller, routes, and removing the route reference from the main router.

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ import stateRoute from "./StatesRoutes.js";
 import cityRoute from "./CityRoutes.js";
 import authRouter from "./Auth.js";
 import userRoute from "./User.js";
-import educationRoute from "./Education.js";
 import jobRoute from "./JobRoutes.js";
 import Reports from "./Reports.js";
 import companyRoute from "./CompanyRoute.js";
@@ -17,14 +16,12 @@ import candidateRoute from "./Candidate.js";
 import scheduleRoute from "./Sedual.js";
 
 const rootRouter = Router();
-
 rootRouter.use('/auth',authRouter);
 rootRouter.use("/admin",adminRoutes);
 rootRouter.use('/country',countryRoute);
 rootRouter.use('/state',stateRoute);
 rootRouter.use('/city',cityRoute);
 rootRouter.use('/user',userRoute);
-rootRouter.use('/Education',educationRoute);
 rootRouter.use('/job',jobRoute);
 rootRouter.use('/reports',Reports);
 rootRouter.use('/company',companyRoute);
@@ -34,8 +31,4 @@ rootRouter.use('/team',teamRoute);
 rootRouter.use('/allLocation',allLocationRoute);
 rootRouter.use('/candidate',candidateRoute);
 rootRouter.use('/schedule',scheduleRoute);
-
-
-
-
 export default rootRouter;
