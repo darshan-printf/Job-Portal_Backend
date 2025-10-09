013dd0cd257acb902614fb2eaac6304ffce6bb61
darshan-printf <darshan.talaviya@egrambook.com>
2025-10-09 11:22:59 +0530

    refactor(email): use Env constant for environment variables
    Improve code readability by using a constant for process.env and include additional project details in email sender name

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,17 +1,18 @@
 import nodemailer from "nodemailer";
+const Env = process.env
 
 export const sendEmail = async (to, subject, text, html) => {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail", // ya SMTP config use kar
       auth: {
-        user: process.env.EMAIL_USER, // apna gmail id
-        pass: process.env.EMAIL_PASS, // app password (gmail ke liye App Password use karna)
+        user: Env.EMAIL_USER, // apna gmail id
+        pass: Env.EMAIL_PASS, // app password (gmail ke liye App Password use karna)
       },
     });
 
     await transporter.sendMail({
-      from: `TalentOS`,
+      from: `${Env.PROJECT_NAME} ${Env.PROJECT_LONCHE_YEAR} ${Env.DEVLOPER_ADDRESS}`,
       to,
       subject,
       text,
