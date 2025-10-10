d6a572ce8bdb208f55d1c327ad9a317c377fced8
darshan-printf <darshan.talaviya@egrambook.com>
2025-10-10 17:22:38 +0530

    style(routes): remove unnecessary empty lines in JobRoutes.js

--- src/routes/JobRoutes.js
+++ src/routes/JobRoutes.js
@@ -11,12 +11,7 @@ jobRoute.get("/get",protect, authorize("admin"), getAllJobs);
 jobRoute.get("/get/:id",protect, authorize("admin", "user"), getJobById);
 jobRoute.delete("/delete/:id",protect, authorize("admin", "user"), deleteJob);
 jobRoute.put("/update",protect, authorize("admin","user"), updateJob);
-
-// add job user
 jobRoute.post("/addPost",protect, authorize("user"), addJobUser);
 jobRoute.get("/getPosts",protect, authorize("user"), getAllJobsByUser);
-
-
-
 
 export default jobRoute;
