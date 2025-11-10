73707308bcb6d64007b0fd846b467f14968b3676
darshan-printf <darshan.talaviya@egrambook.com>
2025-11-10 15:45:10 +0530

    add api

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import protect from "../middleware/authMiddleware.js";
 import { authorize } from "../middleware/authMiddleware.js";
-import { changePassword, getAdminById, updateProfile } from "../controllers/Admin.js";
+import { changePassword, getAdminById, getAdminDetails, updateProfile } from "../controllers/Admin.js";
 import upload from "../middleware/uploadMiddleware.js";
 
 const adminRouter = Router();
 
 adminRouter.post("/updateProfile",protect,authorize("admin","user" ), upload.fields([  { name: 'profileImage', maxCount: 1 }]), updateProfile);
 adminRouter.get("/get/:id",protect,authorize("admin","user"),getAdminById);
-adminRouter.put("/changepassword" ,protect ,authorize("admin","user"),changePassword)
+adminRouter.put("/changepassword" ,protect ,authorize("admin","user"),changePassword);
+adminRouter.get("/chatditails",protect,authorize("admin","user"),getAdminDetails);
+
+
 
 export default adminRouter;
